8451890c97b283de88196a3e3de6ee8991ee2fcf
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-19 05:21:24 +0900

    Updates

---
+++ component---src-pages-404-js-0c60fd2bee1eab955175.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0c60fd2bee1eab955175.js.map
