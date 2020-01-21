35565bb3b64ba71a6d5be36b7fafe95f0f138418
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-21 18:02:11 +0900

    feat: 포스트가 없는 페이지의 SEO 수정

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "@components/layout"
+import SEO from "@components/seo";
 
 const Blog = () => (
   <Layout>
+    <SEO title="Blog" description="No Blogs Yet" />
     <p>No Blogs Yet</p>
   </Layout>
 )
--- src/pages/tag.js
+++ src/pages/tag.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "@components/layout"
+import SEO from "@components/seo";
 
 const Tag = () => (
   <Layout>
+    <SEO title="Tag" description="No Tags Yet" />
     <p>No Tags Yet</p>
   </Layout>
 )
