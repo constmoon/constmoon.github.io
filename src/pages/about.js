5d20076ea0fbdd885fd081887cfe56056b1c2a50
Jiwoo Kang <jiiw.kang@gmail.com>
2020-01-21 18:05:27 +0900

    docs: 소개 문구의 커뮤니티 링크 수정

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,7 +17,7 @@ const AboutPage = ({ data }) => {
         <div className="about-info">
           <h1 className="name">{siteMetadata.name}</h1>
           <a href="mailto:jiiw.kang@gmail.com" className="mail">{siteMetadata.mail}</a>
-          <p className="desc">서비스와 사용자를 잇는 인터페이스를 만들 때 즐거움을 느끼는 웹 개발자입니다. 기술이 세상을 이롭게 할 수 있는 방법에 관심을 두고 있으며, 시빅해킹 커뮤니티 <a href="https://github.com/NullFull">널채움</a>의 조직원으로도 활동 중입니다.</p>
+          <p className="desc">서비스와 사용자를 잇는 인터페이스를 만들 때 즐거움을 느끼는 웹 개발자입니다. 기술이 세상을 이롭게 할 수 있는 방법에 관심을 두고 있으며, 시빅해킹 커뮤니티 <a href="https://nullfull.kr">널채움</a>의 조직원으로도 활동 중입니다.</p>
         </div>
         <ul className="social-links">
           {Object.keys(socialLinks).map((key) => (
