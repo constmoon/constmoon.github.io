03134c69cf14388d76b6f6415fdc76bffcb7ab91
constmoon <jiwoo.link@gmail.com>
2020-04-20 23:56:11 +0900

    no-ssr 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Layout from "@components/layout"
 import SEO from "@components/seo"
-import GoogleSiteVerification from "@components/googleSettings";
-import ProgressBarComponent from "@components/progressbar";
+import NoSSR from 'react-no-ssr'
+import GoogleSiteVerification from "@components/googleSettings"
+import ProgressBarComponent from "@components/progressbar"
 
 const IndexPage = () => (
   <Layout>
     <GoogleSiteVerification />
     <SEO title="Jiwoo Kang" keywords={[`jiwoo`, `constmoon`, `Jiwoo Kang`]} />
-    <ProgressBarComponent />
+    <NoSSR>
+      <ProgressBarComponent />
+    </NoSSR>
   </Layout>
 )
 
