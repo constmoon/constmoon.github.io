7cbf25558da61af33304c15a3d7f6ea5c0ac1ceb
Jiwoo Kang <jiwoo.link@gmail.com>
2021-08-02 02:26:46 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const AboutPage = ({ data }) => {
       <div className="about-info">
         <h1 className="name">{siteMetadata.name}</h1>
         <a href={`mailto:${siteMetadata.mail}`} className="mail">{siteMetadata.mail}</a>
-        <p className="desc">서비스와 사용자를 잇는 인터페이스를 만들 때 즐거움을 느끼는 웹 개발자입니다. 기술로 더 나은 사회를 만들고자 하는 활동에 관심이 많습니다.</p>
+        <p className="desc">서비스와 사용자를 잇는 인터페이스를 만들 때 즐거움을 느끼는 웹 개발자입니다. 기술로 더 나은 세상을 만들고자 하는 활동에 관심이 많습니다.</p>
         <ul className="social-links">
           {Object.keys(socialLinks).map(key => (
             <li key={key}>
