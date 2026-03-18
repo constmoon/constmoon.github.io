2ebc10f52cf031ece03581e4fd4ce3094f80c0e9
constmoon <jiwoo.link@gmail.com>
2026-03-18 22:39:13 +0900

    소개 페이지 스타일 임시 수정

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,27 +26,33 @@ const AboutPage = ({ data }) => {
       <SEO title="About" />
       <div className="about-info">
         <h1 className="name">{siteMetadata.name}</h1>
-        <a href={`mailto:${siteMetadata.mail}`} className="mail">
-          {siteMetadata.mail}
-        </a>
+        <div className="container">
+          <a href={`mailto:${siteMetadata.mail}`} className="mail">
+            {siteMetadata.mail}
+            <span>,</span>
+          </a>
+          <ul className="social-links">
+            {Object.keys(socialLinks).map((key, index) => (
+              <li key={key}>
+                <a
+                  href={socialLinks[key]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {key}
+                  {index < Object.keys(socialLinks).length - 1 && (
+                    <span>,</span>
+                  )}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
         <p className="desc">
           서비스와 사용자를 잇는 인터페이스를 만들 때 즐거움을 느끼는 웹
           개발자입니다. 기술로 더 나은 세상을 만들고자 하는 활동에 관심이
           많습니다.
         </p>
-        <ul className="social-links">
-          {Object.keys(socialLinks).map((key) => (
-            <li key={key}>
-              <a
-                href={socialLinks[key]}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {key}
-              </a>
-            </li>
-          ))}
-        </ul>
       </div>
     </Layout>
   )
